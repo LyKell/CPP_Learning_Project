a7b55ba7d9703e331ee86b3aa1ef224892665dcd
LyKell <kev_dev@hotmail.com>
2021-04-18 19:10:57 +0200

    Task-4 Obj-2 1

---
+++ src/geometry_old.hpp
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <string>
+
+struct Point2D
+{
+    float values[2] {};
+
+    Point2D() {}
+    Point2D(float x, float y) : values { x, y } {}
+
+    float& x() { return values[0]; }
+    float x() const { return values[0]; }
+
+    float& y() { return values[1]; }
+    float y() const { return values[1]; }
+
+    Point2D& operator+=(const Point2D& other)
+    {
+        x() += other.x();
+        y() += other.y();
+        return *this;
+    }
+
+    Point2D& operator*=(const Point2D& other)
+    {
+        x() *= other.x();
+        y() *= other.y();
+        return *this;
+    }
+
+    Point2D& operator*=(const float scalar)
+    {
+        x() *= scalar;
+        y() *= scalar;
+        return *this;
+    }
+
+    Point2D operator+(const Point2D& other) const
+    {
+        Point2D result = *this;
+        result += other;
+        return result;
+    }
+
+    Point2D operator*(const Point2D& other) const
+    {
+        Point2D result = *this;
+        result *= other;
+        return result;
+    }
+
+    Point2D operator*(const float scalar) const
+    {
+        Point2D result = *this;
+        result *= scalar;
+        return result;
+    }
+};
+
+struct Point3D
+{
+    std::array<float, 3> values;
+
+    Point3D() {}
+    Point3D(float x, float y, float z) : values { x, y, z } {}
+
+    float& x() { return values[0]; }
+    float x() const { return values[0]; }
+
+    float& y() { return values[1]; }
+    float y() const { return values[1]; }
+
+    float& z() { return values[2]; }
+    float z() const { return values[2]; }
+
+    Point3D& operator+=(const Point3D& other)
+    {
+        std::transform(values.begin(), values.end(), other.values.begin(), values.begin(), std::plus<float>());
+        return *this;
+    }
+
+    Point3D& operator-=(const Point3D& other)
+    {
+        std::transform(values.begin(), values.end(), other.values.begin(), values.begin(), std::minus<float>());
+        return *this;
+    }
+
+    Point3D& operator*=(const float scalar)
+    {
+        std::transform(values.begin(), values.end(), values.begin(), [scalar](float val){ return val * scalar; });
+        return *this;
+    }
+
+    Point3D operator+(const Point3D& other) const
+    {
+        Point3D result = *this;
+        result += other;
+        return result;
+    }
+
+    Point3D operator-(const Point3D& other) const
+    {
+        Point3D result = *this;
+        result -= other;
+        return result;
+    }
+
+    Point3D operator*(const float scalar) const
+    {
+        Point3D result = *this;
+        result *= scalar;
+        return result;
+    }
+
+    Point3D operator-() const { return Point3D { -x(), -y(), -z() }; }
+
+    float length() const
+    {
+        return std::sqrt(std::accumulate(values.begin(), values.end(), 0.f, [](float v1, float v2){ return v1 + (v2 * v2); }));
+    }
+
+    float distance_to(const Point3D& other) const { return (*this - other).length(); }
+
+    Point3D& normalize(const float target_len = 1.0f)
+    {
+        const float current_len = length();
+        if (current_len == 0)
+        {
+            throw std::logic_error("cannot normalize vector of length 0");
+        }
+
+        *this *= (target_len / current_len);
+        return *this;
+    }
+
+    Point3D& cap_length(const float max_len)
+    {
+        assert(max_len > 0);
+
+        const float current_len = length();
+        if (current_len > max_len)
+        {
+            *this *= (max_len / current_len);
+        }
+
+        return *this;
+    }
+
+    std::string to_string()
+    {
+        return "(" + std::accumulate(values.begin() + 1, values.end(), std::to_string(values[0]),
+            [](std::string v1, float v2)
+            {
+                return std::move(v1) + ", " + std::to_string(v2);
+            }) + ")";
+    }
+};
+
+// our 3D-coordinate system will be tied to the airport: the runway is parallel to the x-axis, the z-axis
+// points towards the sky, and y is perpendicular to both thus,
+// {1,0,0} --> {.5,.5}   {0,1,0} --> {-.5,.5}   {0,0,1} --> {0,1}
+inline Point2D project_2D(const Point3D& p)
+{
+    return { .5f * p.x() - .5f * p.y(), .5f * p.x() + .5f * p.y() + p.z() };
+}
